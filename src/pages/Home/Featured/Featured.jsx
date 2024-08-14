c0a1dd2f70f69efc5a64f6157b67822f47ccea11
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-08-14 22:56:37 +0600

    added new section testimonial

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,28 +4,32 @@ import "./Featured.css";
 
 const Featured = () => {
   return (
-    <div className="featured-item text-white pt-8 my-20">
-      <SectionTitle
-        subheading="check it out"
-        heading="Featured Item"
-      ></SectionTitle>
-      <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
-        <div>
-          <img src={featuredImg} alt="" />
-        </div>
-        <div className="md:ml-10">
-          <p>Aug 20, 2029</p>
-          <p className="uppercase">Where i ger some?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-            exercitationem dicta voluptas voluptates ipsum explicabo suscipit
-            consectetur repudiandae harum illum ut corporis unde blanditiis
-            beatae praesentium necessitatibus repellat commodi nemo consequatur,
-            placeat amet omnis maiores? Dolores facilis optio repellat, rem
-            totam animi, ipsum placeat exercitationem, culpa omnis neque
-            voluptatem quam.
-          </p>
-          <button className="btn btn-outline">Order Now</button>
+    <div className="featured-item bg-fixed  text-white my-20">
+      <div className="bg-slate-500 bg-opacity-60 py-8">
+        <SectionTitle
+          subheading="check it out"
+          heading="Featured Item"
+        ></SectionTitle>
+        <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+          <div>
+            <img src={featuredImg} alt="" />
+          </div>
+          <div className="md:ml-10">
+            <p>Aug 20, 2029</p>
+            <p className="uppercase">Where i ger some?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              exercitationem dicta voluptas voluptates ipsum explicabo suscipit
+              consectetur repudiandae harum illum ut corporis unde blanditiis
+              beatae praesentium necessitatibus repellat commodi nemo
+              consequatur, placeat amet omnis maiores? Dolores facilis optio
+              repellat, rem totam animi, ipsum placeat exercitationem, culpa
+              omnis neque voluptatem quam.
+            </p>
+            <button className="btn btn-outline border-0 border-b-4">
+              Order Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
