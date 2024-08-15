4fd1c55badac7fe14c2e70f5c15e875a024c78c6
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-08-15 22:37:34 +0600

    Display different menu category items with cover

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,14 +1,45 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertimg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaimg from "../../../assets/menu/pizza-bg.jpg";
+import saladimg from "../../../assets/menu/salad-bg.jpg";
+import soupimg from "../../../assets/menu/soup-bg.jpg";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="Our Menu"></Cover>
+      <SectionTitle
+        subheading="Don't Miss"
+        heading="Today's Offer"
+      ></SectionTitle>
+      {/* offerd menu items  */}
+      <MenuCategory items={offered}></MenuCategory>
+      {/* dessert menu items  */}
+      <MenuCategory
+        items={desserts}
+        title="Dessert"
+        img={dessertimg}
+      ></MenuCategory>
+      {/* Pizza menu items  */}
+      <MenuCategory items={pizza} title="Pizza" img={pizzaimg}></MenuCategory>
+      {/* Salad menu items  */}
+      <MenuCategory items={salad} title="Salad" img={saladimg}></MenuCategory>
+      {/* Soup menu items  */}
+      <MenuCategory items={soup} title="Soup" img={soupimg}></MenuCategory>
     </div>
   );
 };
