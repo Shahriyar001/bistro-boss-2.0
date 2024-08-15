049efffacb7e245c765f2ddcd8007cce8f35d59d
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-08-15 23:59:19 +0600

    added tab

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { useState } from "react";
+import orderCoverImg from "../../../assets/shop/banner2.jpg";
+import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <div>
-      <h1>order</h1>
+      <Cover img={orderCoverImg} title="Order Food"></Cover>
+      <Tabs defaultIndex={1} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
     </div>
   );
 };
