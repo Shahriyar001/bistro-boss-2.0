271288a8392c3f1e9eeb93799c6c3b1c3eb1389f
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-08-16 22:17:13 +0600

    cunnect with server and get data

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -10,11 +10,13 @@ const FoodCard = ({ item }) => {
       <p className="absolute right-0 mt-4 mr-4 px-3 py-1 bg-slate-900 text-white">
         ${price}
       </p>
-      <div className="card-body text-white text-center">
+      <div className="card-body text-white flex flex-xol items-center">
         <h2 className="text-xl font-bold">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to Cart</button>
+          <button className="btn btn-outline bg-slate-200 border-orange-400 border-0 border-b-4">
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
