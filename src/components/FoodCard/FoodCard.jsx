ddd73fbb6acfc398806d81103031aa77dade005e
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-08-22 20:20:34 +0600

    Save Cart data to the server using axios

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,11 +1,49 @@
+import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const FoodCard = ({ item }) => {
-  const { image, price, recipe, name } = item;
+  const { image, price, recipe, name, _id } = item;
   const { user } = useAuth();
-
-  const handleAddRoCart = (food) => {
-    console.log(food);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleAddToCart = (food) => {
+    if (user && user.email) {
+      const cartItem = {
+        menuId: _id,
+        email: user.email,
+        name,
+        image,
+        price,
+      };
+      axios.post("http://localhost:5000/carts", cartItem).then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: `${name} added to your cart`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    } else {
+      Swal.fire({
+        title: "please login first",
+        text: "To add to cart, you have to login first!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, Login!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    }
   };
 
   return (
@@ -21,7 +59,7 @@ const FoodCard = ({ item }) => {
         <p>{recipe}</p>
         <div className="card-actions justify-end">
           <button
-            onClick={() => handleAddRoCart(item)}
+            onClick={() => handleAddToCart(item)}
             className="btn btn-outline bg-slate-200 border-orange-400 border-0 border-b-4"
           >
             Add to Cart
