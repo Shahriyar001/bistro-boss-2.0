6be41d17515fb11df4206d1375a951af8e316a7a
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-08-30 23:57:38 +0600

    added new component for payment

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Payment = () => {
+  return (
+    <div>
+      <SectionTitle
+        heading="Payment"
+        subheading="Please pay to eat"
+      ></SectionTitle>
+      <div></div>
+    </div>
+  );
+};
+
+export default Payment;
