94c9be4b7d565ff9ac3939ce4ecab9efb2ad7ecd
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-01 23:51:42 +0600

    Display Payment History

--- src/pages/Dashboard/PaymentHistory/PaymentHistroy.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistroy.jsx
@@ -1,5 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const PaymentHistroy = () => {
-  return <div>paymnet</div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user.email}`);
+      return res.data;
+    },
+  });
+  console.log(payments);
+
+  return (
+    <div>
+      <h2 className="text-xl">Total Payments: {payments.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Price</th>
+              <th>TransactionId</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {payments.map((payment, i) => (
+              <tr key={payment._id}>
+                <th>{(i += 1)}</th>
+                <td>{payment.price}</td>
+                <td>{payment.transactionId}</td>
+                <td>{payment.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentHistroy;
